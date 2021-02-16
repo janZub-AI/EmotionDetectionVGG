d0951ceceaac2f877c040ab9a52d9950ae716be8
janzu <jan.zubrycki@gmail.com>
2021-02-16 13:49:59 +0100

    Implementation of VGG-B*

--- model.py
+++ model.py
@@ -1,35 +1,91 @@
 from keras.models import Sequential
 from keras.models import load_model
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, Activation
 from keras.layers import Flatten, Dropout, Dense, BatchNormalization
 
 from keras.layers.advanced_activations import PReLU
 from keras.optimizers import Adam
-from keras.initializers import HeNormal
+from keras.initializers import GlorotNormal, HeNormal
 
 from kerastuner import HyperModel, HyperParameters
-
+import uuid
 class ConcreteModel(HyperModel):
+    """ VGG-B without last C512_C512_P block,
+        with relu and glorot,
+        extra dropout and BatchNormalization
+        not sure about opt
+    """
 
     def __init__(self, num_classes, input_shape):
         self.num_classes = num_classes
         self.input_shape = input_shape
 
     def build(self, hp):
+
+        # The same initializer
+        initializer = GlorotNormal()
+        # The same activation for all hidden is relu
+        activation = Activation('relu')
+
         model = Sequential()
+         
+        model.add(Conv2D(filters = 64, kernel_size = (3, 3), padding = 'same', input_shape = self.input_shape, kernel_initializer=initializer))
+        model.add(BatchNormalization())
+        model.add(activation)
+        model.add(Conv2D(filters = 64, kernel_size = (3, 3), padding = 'same', kernel_initializer=initializer))
+        model.add(BatchNormalization())
+        model.add(activation)
+        model.add(MaxPooling2D((2, 2)))
+
+        model.add(Dropout(0.5))
+        model.add(Conv2D(filters = 128, kernel_size = (3, 3), padding = 'same', kernel_initializer=initializer))
+        model.add(BatchNormalization())
+        model.add(activation)
+        model.add(Conv2D(filters = 128, kernel_size = (3, 3), padding = 'same', kernel_initializer=initializer))
+        model.add(BatchNormalization())
+        model.add(activation)
+        model.add(MaxPooling2D((2, 2)))
 
 
+        model.add(Dropout(0.5))
+        model.add(Conv2D(filters = 256, kernel_size = (3, 3), padding = 'same', kernel_initializer=initializer))
+        model.add(BatchNormalization())
+        model.add(activation)
+        model.add(Conv2D(filters = 256, kernel_size = (3, 3), padding = 'same', kernel_initializer=initializer))
+        model.add(BatchNormalization())
+        model.add(activation)
+        model.add(MaxPooling2D((2, 2)))
+
+
+        model.add(Dropout(0.5))
+        model.add(Conv2D(filters = 512, kernel_size = (3, 3), padding = 'same', kernel_initializer=initializer))
+        model.add(BatchNormalization())
+        model.add(activation)
+        model.add(Conv2D(filters = 512, kernel_size = (3, 3), padding = 'same', kernel_initializer=initializer))
+        model.add(BatchNormalization())
+        model.add(activation)
+        model.add(MaxPooling2D((2, 2)))
+        
+        model.add(Flatten())
+        
+        model.add(Dropout(0.5))
+        model.add(Dense(1024))
+        model.add(BatchNormalization())
+        model.add(activation)
+        
+        model.add(Dense(self.num_classes, activation='softmax'))
+
+        model.compile(optimizer = Adam())
         return model
 
     def define_hp(hp_model = None):
-        hp = HyperParameters()
-
+        hp = new ConcreateHyperParameters()
         return hp
 
     def generate_model_name(iterable, **kwarg):
-        hp = kwarg['hp']    
+        #hp = kwarg['hp']    
             
-        return name
+        return f'VGG-B*{uuid.uuid4().hex}'
 
 class ConcreateHyperParameters():
     """!Important! Ensure that if min provided, then max > min is specified either.
